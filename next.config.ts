c76a8c5ca3682848d7b8a14ddbbbd393d00666ba
Isam Ahmed <isam.ahmedh8@gmail.com>
2026-09-23 23:20:35 +0300

    package fix for hosting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["rb1ttjanjn.preview.c24.airoapp.ai"],
 };
 
 export default nextConfig;
